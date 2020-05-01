4adfbadec7bc88ac9c39ea793979a5ecda0bcf53
Pawel Waniek <pawel.waniek95@gmail.com>
2020-05-01 13:10:32 +0200

    Zadanie 2 - napisanie programu do obliczania silni dla liczb 9,12,20 (pętla for)

--- WSB_C_2020_Z1.cs
+++ WSB_C_2020_Z1.cs
@@ -6,8 +6,26 @@ namespace WSB_C_2020_Z1
     {
         static void Main(string[] args)
         {
-            // Zadanie 1
-            Console.WriteLine("Witaj świecie");
+            // Zadanie 2 - kalkulator silni
+
+            // Tablica pomocnicza przechowującą "silnie" do obliczenia
+            int[] silnie = { 9, 12, 20 };
+
+            // Pętla iterująca po tablicy silni
+            for (int i = 0; i < silnie.Length; i++)
+            {
+                // Zmienna przechowująca wynik
+                ulong wynik = 1;
+
+                // Pętla licząca silnie
+                for (int j = 1; j <= silnie[i]; j++)
+                {
+                    // Konwersja jawna do ulong, żeby zmienna "j" była w tym samym typie co nasz wynik
+                    wynik = wynik * (ulong)j;
+                }
+
+                Console.WriteLine("Wartość " + silnie[i] + "! jest równa: " + wynik);
+            }
 
             // Wstrzymanie okna 
             Console.WriteLine("Naciśnij enter aby wyjść");
