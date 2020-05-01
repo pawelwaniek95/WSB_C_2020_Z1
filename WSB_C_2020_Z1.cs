1f0b8c8e69c341c3f289c2e3ef15470536034d74
Pawel Waniek <pawel.waniek95@gmail.com>
2020-05-01 13:11:27 +0200

    Zadanie 2.1 - zmodyfikowanie programu, by pobierał parametr z klawiatury

--- WSB_C_2020_Z1.cs
+++ WSB_C_2020_Z1.cs
@@ -6,25 +6,45 @@ namespace WSB_C_2020_Z1
     {
         static void Main(string[] args)
         {
-            // Zadanie 2 - kalkulator silni
+            // Zadanie 2.1 - kalkulator silni - z możliwością wpisywanie silni do obliczenia
 
-            // Tablica pomocnicza przechowującą "silnie" do obliczenia
-            int[] silnie = { 9, 12, 20 };
+            // Prośba o liczbę
+            Console.Write("Wpisz z jakiej liczby chcesz policzyć silnię (max 21!): ");
 
-            // Pętla iterująca po tablicy silni
-            for (int i = 0; i < silnie.Length; i++)
+            try
             {
+                // Przechwycenie ewentualnych wyjątków, by program się nie zawiesił
+
+                // Odczyt liczby od użytkownika
+                int silnia = int.Parse(Console.ReadLine());
+
+                if (silnia >= 21)
+                {
+                    Console.WriteLine("Liczba z poza zakresu, niby gdzie miałbym włożyć wynik?");
+                    // Wyjście z programu, bo nie da się policzyć takiej silni
+                    return;
+                }
+
                 // Zmienna przechowująca wynik
                 ulong wynik = 1;
 
                 // Pętla licząca silnie
-                for (int j = 1; j <= silnie[i]; j++)
+                for (int i = 1; i <= silnia; i++)
                 {
                     // Konwersja jawna do ulong, żeby zmienna "j" była w tym samym typie co nasz wynik
-                    wynik = wynik * (ulong)j;
+                    wynik = wynik * (ulong)i;
                 }
 
-                Console.WriteLine("Wartość " + silnie[i] + "! jest równa: " + wynik);
+                Console.WriteLine("Wartość " + silnia + "! jest równa: " + wynik);
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Nie poprawny format liczby, tylko liczby proszę!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Coś poszło nie tak ;(");
             }
 
             // Wstrzymanie okna 
