e593fc04463c415a130e47041ef9679a49bb207b
Pawel Waniek <pawel.waniek95@gmail.com>
2020-05-01 13:13:57 +0200

    Zadanie 3.1 - modyfikacja kodu, by można było wybrać operację dodawania lub odejmowania (instrukcja if)

--- WSB_C_2020_Z1.cs
+++ WSB_C_2020_Z1.cs
@@ -6,18 +6,50 @@ namespace WSB_C_2020_Z1
     {
         static void Main(string[] args)
         {
-            // Zadanie 3
+            // Zadanie 3 podpunkt 1
+
+            // Flaga pokazująca, czy liczby są poprawnie pobrane
+            int badFLag = 0;
 
             // Pobranie parametrów od użytkownika
-            Console.WriteLine("### Mnożenie dwóch liczb ###");
-            Console.WriteLine("Podaj pierwszą liczbę");
-            int liczba1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Podaj drugą liczbę");
-            int liczba2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("### Kalkulator ###");
+            Console.Write("Podaj pierwszą liczbę: ");
+            int liczba1 = 0;
 
-            int wynik = liczba1 * liczba2;
-            Console.WriteLine(liczba1 + " * " + liczba2 + " = " + wynik);
+            // Gdy wystąpi bład parsowania, flaga zostanie ustawiona
+            if (!int.TryParse(Console.ReadLine(), out liczba1))
+            {
+                badFLag = 1; Console.WriteLine("Błąd parsowania pierwszej liczby");
+            }
 
+            Console.Write("Podaj drugą liczbę: ");
+            int liczba2 = 0;
+            if (!int.TryParse(Console.ReadLine(), out liczba2)) { badFLag = 1; Console.WriteLine("Błąd parsowania drugiej liczby"); }
+
+            Console.WriteLine("Jaką operację chcesz wykonać?");
+            Console.WriteLine("1 - dodawanie");
+            Console.WriteLine("2 - odejmowanie");
+            Console.Write("Operacja: ");
+
+            int decyzja = 0;
+            if (!int.TryParse(Console.ReadLine(), out decyzja)) { badFLag = 1; Console.WriteLine("Błąd parsowania działania"); }
+
+            int wynik = 0;
+
+            if (badFLag == 0)
+            {
+                if (decyzja == 1)
+                {
+                    wynik = liczba1 + liczba2;
+                    Console.WriteLine(liczba1 + " + " + liczba2 + " = " + wynik);
+                }
+                else if (decyzja == 2)
+                {
+                    wynik = liczba1 - liczba2;
+                    Console.WriteLine(liczba1 + " - " + liczba2 + " = " + wynik);
+                }
+
+            }
         }
     }
 }
