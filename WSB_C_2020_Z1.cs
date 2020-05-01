add1c989743c84b22e06601d9717190b3f5bd079
Pawel Waniek <pawel.waniek95@gmail.com>
2020-05-01 13:08:59 +0200

    Zadanie 1 - napisanie programu do wyświetlania napisu "Hello World!"

---
+++ WSB_C_2020_Z1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WSB_C_2020_Z1
+{
+    class WSB_C_2020_Z1
+    {
+        static void Main(string[] args)
+        {
+            // Zadanie 1
+            Console.WriteLine("Witaj świecie");
+
+            // Wstrzymanie okna 
+            Console.WriteLine("Naciśnij enter aby wyjść");
+            Console.ReadLine();
+        }
+    }
+}
